ef0896b10b0ef79c84dc28a229a34df5acd1e6cf
miya19951025 <asobimakuri@gmail.com>
2020-02-23 23:43:54 +0900

    どこつぶ一回目

---
+++ docoTsubu/src/model/Mutter.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Mutter  {
+	private String userName; //ユーザー名
+	private String text; //つぶやき内容
+	public Mutter() {}
+	public Mutter(String userName, String text) {
+		this.userName = userName;
+		this.text = text;
+
+	}
+	public String getUserName() {return userName;}
+	public String getText() { return text;}
+
+
+}
